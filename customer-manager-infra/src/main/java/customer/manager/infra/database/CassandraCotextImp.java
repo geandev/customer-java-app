2730e5ca8e46583db39d0a12846af586886a2082
gean <geanalexandre@hotmail.com>
2017-03-22 00:41:49 -0300

    working in progress

--- customer-manager-infra/src/main/java/customer/manager/infra/database/CassandraCotextImp.java
+++ customer-manager-infra/src/main/java/customer/manager/infra/database/CassandraCotextImp.java
@@ -7,17 +7,7 @@ import com.datastax.driver.core.Session;
 
 @Service
 public class CassandraCotextImp implements ContextBase {
-
-  private static final String SERVER_IP = "127.0.0.1";
-  private static final String DATABASE = "customer";
-  private final Session session;
-
-  public CassandraCotextImp() {
-    Cluster cluster = Cluster.builder().addContactPoint(SERVER_IP).build();
-    session = cluster.connect(DATABASE);
-  }
-
   public Session getSession() {
-    return this.session;
+    return null;
   }
 }
--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 gradle build
+gradle bootRun -p customer-manager-oauths &
 gradle bootRun -p customer-manager-server-eureka &
 gradle bootRun -p customer-manager-rest
